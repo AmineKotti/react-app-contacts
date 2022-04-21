d22aebe57cd642c187bc80f1b20c62010748ed7f
AmineKotti <mohamedamine.kotti@esprit.tn>
2022-04-21 18:44:48 +0100

    add contacts and display a list of contacts added

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,21 @@
 import './App.css';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
 
- const contacts = [
-   {
-     id : "1",
-     name : "Dipesh",
-     email : "malvia@gmail.com",
-   },
-   {
-    id : "2",
-    name : "Nikesh",
-    email : "nicks@gmail.com",
-   }
- ]
-
+  const addContactHandler = (contact) => {
+      console.log(contact)
+      setContacts([...contacts, contact])
+  }
 
   return (
    <div className='ui container'>
      <Header />
-     <AddContact />
+     <AddContact  addContactHandler = {addContactHandler}/>
      <ContactList  contacts = {contacts}/>
    </div>
   );
