a54a4178b22a18c5cd065d79a7711604c9bfec1e
AmineKotti <mohamedamine.kotti@esprit.tn>
2022-04-21 17:48:39 +0100

    add contact and display list of contacts

---
+++ src/components/ContactCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const ContactCard = (props) => {
+    const { name, email } =  props.contact;
+    return (
+        <div className="item">
+           <div className="content">
+               <div className="header">{ name }</div>
+               <div>{ email }</div>
+           </div>
+           <i style={{color:"red", marginTop :"5px"}} className="trash alternate outline icon"></i>
+        </div>
+      );
+}
+
+
+
+export default ContactCard;
